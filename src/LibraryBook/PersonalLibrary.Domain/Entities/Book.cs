1a1f91e81cd8c5d593f9be1b6ef5fb400fc0d680
jonatas.paz <jonatas.paz@imusica.com.br>
2025-04-24 17:40:37 -0300

    subindo alteração na entidade book para media

--- src/LibraryBook/PersonalLibrary.Domain/Entities/Book.cs
+++ src/LibraryBook/PersonalLibrary.Domain/Entities/Book.cs
@@ -6,10 +6,10 @@ using System.Threading.Tasks;
 
 namespace LibraryBook.Domain.Entities
 {
-    public class Book : BaseEntity
+    public class Media : BaseEntity
     {
         public string Title { get; set; }
-        public long? IsbnNumber { get; set; }
+        public string Type { get; set; }
         public string Author { get; set; }
         public string Editor { get; set; }
         public string Synopsis { get; set; }
---
+++ src/LibraryBook/PersonalLibrary.Domain/Enum/Category.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalLibrary.Domain.Enum
+{
+    public enum Category
+    {
+        Book = 1,
+        Game = 2
+    }
+}
